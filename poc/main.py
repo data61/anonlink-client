330c27770fba7098f7c62a7838b22ff7ce2955d7
Brian Thorne <brian@thorne.link>
2019-10-22 10:38:31 +1100

    Combine the candidate blocks

--- poc/main.py
+++ poc/main.py
@@ -14,9 +14,9 @@ def compute_candidate_block_filter(data, blocking_config):
     return candidate_block_filter_from_signatures(signatures, config)
 
 
+
 def run():
-    data = list(clkhash.randomnames.NameList(2).names)
-    print(data[0])
+
 
     blocking_config = {
         'signature': {
@@ -37,8 +37,16 @@ def run():
 
     }
 
-    res = compute_candidate_block_filter(data, blocking_config)
-    print(res)
+    data = list(clkhash.randomnames.NameList(100).names)
+    print("Example PII", data[0])
+    dp1_candidate_block_filter = compute_candidate_block_filter(data[:75], blocking_config)
+    dp2_candidate_block_filter = compute_candidate_block_filter(data[50:], blocking_config)
+    print("Candidate block filter dp1:", dp1_candidate_block_filter)
+    print("Candidate block filter dp2:", dp2_candidate_block_filter)
+
+    block_filter = compute_blocking_filter([dp1_candidate_block_filter, dp2_candidate_block_filter])
+    print("Block filter:", block_filter)
+    
 
 
 if __name__ == '__main__':
