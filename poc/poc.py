7473dd7697cf9c02ff66707ca498f7774c77109b
Brian Thorne <brian@thorne.link>
2019-10-21 16:14:08 +1100

    Start of a poc...

--- poc/poc.py
+++ poc/poc.py
@@ -1,6 +1,43 @@
+import clkhash
+
+
+def compute_candidate_block_filter(data, blocking_config):
+    signature_config = blocking_config['signature']
+    filter_config = blocking_config['filter']
+    config = blocking_config['candidate-blocking-filter']
+
+    candidate_signatures = compute_signatures(data, signature_config)
+    signatures = filter_signatures(candidate_signatures, filter_config)
+    return candidate_block_filter_from_signatures(signatures, config)
 
 
 
+def run():
+    data = list(clkhash.randomnames.NameList(100).names)
+
+    print(data[0])
+
+    blocking_config = {
+        'signature': {
+            'type': 'feature-value',
+            'feature-index': 3
+        },
+        'filter': {
+            'type': 'none'
+            # 'type': 'frequency',
+            # 'min': 2,
+            # 'max': 1000
+        },
+        'candidate-blocking-filter': {
+            'type': 'dummy',
+            'filter-length': 2
+        }
+
+    }
+
+    compute_candidate_block_filter(data, blocking_config)
 
 
 
+if __name__ == '__main__':
+    run()
