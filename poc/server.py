98d1c071c082b985e2c07ad8868668c5fb990fa2
wilko <wilko.henecka@data61.csiro.au>
2019-10-22 22:53:01 +1100

    added solving step using anonlink

--- poc/server.py
+++ poc/server.py
@@ -1,6 +1,6 @@
-from typing import Sequence
+from typing import Sequence, List, Dict
 import numpy as np
-
+from bitarray import bitarray
 import anonlink
 
 
@@ -10,12 +10,29 @@ def compute_blocking_filter(candidate_blocking_filters: Sequence[np.ndarray], th
     return blocking_filter
 
 
-def mapping_from_clks(clks_a, clks_b, threshold):
-    results_candidate_pairs = anonlink.candidate_generation.find_candidate_pairs(
-            [clks_a, clks_b],
-            anonlink.similarities.dice_coefficient,
-            threshold
-    )
-    solution = anonlink.solving.greedy_solve(results_candidate_pairs)
-    print('Found {} matches'.format(len(solution)))
-    return {a:b for ((_, a),(_, b)) in solution}
+def solve(encodings: Sequence[List[bitarray]], rec_to_blocks: Sequence[Dict[int, list]], threshold: float = 0.8):
+    """ entity resolution, baby
+
+    calls anonlink to do the heavy lifting.
+
+    :param encodings: a sequence of lists of Bloom filters (bitarray). One for each data provider
+    :param rec_to_blocks: a sequence of dictionaries, mapping a record id to the list of blocks it is part of. Again,
+                          one per data provider, same order as encodings.
+    :param threshold: similarity threshold for solving
+    :return: same as the anonlink solver.
+             An sequence of groups. Each group is an sequence of
+             records. Two records are in the same group iff they represent
+             the same entity. Here, a record is a two-tuple of dataset index
+             and record index.
+    """
+    def my_blocking_f(ds_idx, rec_idx, _):
+        return rec_to_blocks[ds_idx][rec_idx]
+
+    candidate_pairs = anonlink.candidate_generation.find_candidate_pairs(
+        encodings,
+        anonlink.similarities.dice_coefficient,
+        threshold=threshold,
+        blocking_f=my_blocking_f)
+
+    return anonlink.solving.greedy_solve(candidate_pairs)
+
