45e94aa2ded4aee1c6631f1b4e3c1065a1e9f830
Brian Thorne <brian@thorne.link>
2019-10-22 13:24:35 +1100

    Docstring for signature to record mapping

--- poc/main.py
+++ poc/main.py
@@ -6,6 +6,17 @@ from poc.signature_generator import compute_signatures
 
 
 def compute_candidate_block_filter(data, blocking_config):
+    """
+
+    :param data:
+    :param blocking_config:
+    :return:
+        A 3-tuple containing:
+        - the candidate block filter (CBF) and
+        - a dict mapping block index (e.g. index in the cbf) to the list of
+          signatures that are associated with that cbf location, and
+        - a dict mapping signatures to records
+    """
     signature_config = blocking_config['signature']
     filter_config = blocking_config['filter']
     config = blocking_config['candidate-blocking-filter']
@@ -15,8 +26,22 @@ def compute_candidate_block_filter(data, blocking_config):
     return candidate_block_filter_from_signatures(signatures, config)
 
 
-def create_reverse_index(block_filter, cbf_map_1, param):
-    print("TODO")
+def create_reverse_index(block_filter, bf_map, sig_rec_map):
+    """
+    Two methods have been implemented that map to a list of records.
+
+    - index based blocking: creates a block for every high bit in the
+      bloom filter.
+    - signature based blocking: creates a block for every individual
+      signature.
+
+    :param block_filter:
+    :param bf_map:
+    :param param:
+    :return:
+    """
+
+
 
 
 def run_gender_blocking():
@@ -52,7 +77,8 @@ def run_gender_blocking():
     block_filter = compute_blocking_filter((dp1_candidate_block_filter, dp2_candidate_block_filter))
     print("Block filter:", block_filter)
 
-    create_reverse_index(block_filter, cbf_map_1, 'todo GS? signature -> record mapping')
+    dp1_index = create_reverse_index(block_filter, cbf_map_1, 'todo GS? signature -> record mapping')
+
 
 
 if __name__ == '__main__':
