7e4bf2ff7847c00d007ccfbedded76c949d9a3f3
wilko <wilko.henecka@data61.csiro.au>
2019-10-22 22:52:34 +1100

    I need a different kind of reverse index

--- poc/reverse_index.py
+++ poc/reverse_index.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def create_reverse_index(block_filter, cbf_map, sig_rec_map, config):
     """
 
@@ -32,4 +35,41 @@ def _single_index_reverse_index(blocking_filter, cbf_map, sig_to_record_map, con
     for i in range(len(blocking_filter)):
         if blocking_filter[i] == 1:
             block_map[i] = [y for x in cbf_map[i] for y in sig_to_record_map[x]]
+    return block_map
+
+
+def create_block_list_lookup(block_filter, cbf_map, sig_rec_map, config):
+    """
+
+    :param block_filter: The combined blocking filter - a numpy bool array.
+    :param cbf_map: Dict mapping blocking filter index to list of signatures.
+    :param sig_rec_map: Dict mapping signatures to records.
+    :return:
+        Dict mapping record id to list of blocks it belongs to.
+    """
+    method_type = config.get('type', 'not provided')
+    if method_type == 'not provided':
+        raise ValueError("reverse-index type not provided.")
+
+    if method_type == 'group-single-index':
+        return _single_index_block_list_lookup(block_filter, cbf_map, sig_rec_map)
+    else:
+        raise ValueError("reverse index type '{}' is not recognized.".format(method_type))
+
+
+def _single_index_block_list_lookup(blocking_filter, cbf_map, sig_to_record_map):
+    """
+    Each 1 in the blocking filter represents a block.
+    :param blocking_filter:
+    :param cbf_map:
+    :param sig_to_record_map:
+    :return:
+        Dict mapping record id to list of blocks it belongs to.
+    """
+    block_map = defaultdict(list)
+    for i in range(len(blocking_filter)):
+        if blocking_filter[i] == 1:
+            for x in cbf_map[i]:
+                for y in sig_to_record_map[x]:
+                    block_map[y].append(i)
     return block_map
