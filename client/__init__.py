3cd5198839a7aa0c93fc0897761eec5f8aea086f
Joyce Wang <xwan8513@uni.sydney.edu.au>
2019-12-02 16:39:27 +1100

    Migrate cli and rest_client from clkhash and tests for CLI all passed

---
+++ client/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+from .rest_client import RestClient, ServiceError
+
+try:
+    __version__ = pkg_resources.get_distribution('client').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "development"
+
+__author__ = "Data61"
