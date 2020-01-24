56967fb38f339b09a7ade5f05a193860b4313259
Joyce Wang <xwan8513@uni.sydney.edu.au>
2020-01-24 14:27:56 +1100

    Add more tests on utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,36 @@
+"""Test utils."""
+import io
+import os
+import unittest
+from bitarray import bitarray
+from clkhash.serialization import serialize_bitarray
+
+from client.utils import deserialize_filters, generate_candidate_blocks_from_csv
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_deserialize_bitarray(self):
+        """Test deserialize bitarray."""
+        # create simple bitarray bloom filter
+        lst = [True, False, True, False] * 2
+        bf = bitarray(lst)
+        serialized_bf = serialize_bitarray(bf)
+        deserialized_bf = deserialize_filters([serialized_bf])
+        assert deserialized_bf == [bf]
+
+
+    def test_generate_candidate_blocks_from_csv_exception(self):
+        """Test exception case for generate candidate blocks from csv."""
+        data = [[1, 'Joyce'],
+                [2, 'Fred']]
+        input_f = io.StringIO(str(data))
+        schema_f = open(os.path.join(
+            os.path.dirname(__file__),
+            'testdata/bad-schema-v1.json'))
+
+
+        # test invalid header
+        header = 'Header'
+        with self.assertRaises(ValueError):
+            generate_candidate_blocks_from_csv(input_f, schema_f, header)
